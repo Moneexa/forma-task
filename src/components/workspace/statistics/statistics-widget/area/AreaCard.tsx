32093cac7584dc9729ccd16b65bc097d33de5317
Moneexa <14besemsyed@seecs.edu.pk>
2024-08-29 18:14:07 +0200

    fixed area upon change-solution

--- src/components/workspace/statistics/statistics-widget/area/AreaCard.tsx
+++ src/components/workspace/statistics/statistics-widget/area/AreaCard.tsx
@@ -1,10 +1,13 @@
+import { useContext } from "react"
 import { card, cardBody, cardDivider, cardFooter, subHeading } from "./AreaCard.css"
+import { SolutionsContext } from "../../../../../store/Solutions"
 
-export default function AreaCard(props: { area: { value: number } }) {
+export default function AreaCard(props: { area: { value: number, proposedSolution: number } }) {
     const { area } = props
+    const { selectedSolIndx } = useContext(SolutionsContext)
     return <div className={card}>
         <div className={cardBody}>
-            <div className={subHeading}>{area.value.toFixed(3)}</div>
+            <div className={subHeading}>{selectedSolIndx == area.proposedSolution ? area.value.toFixed(3) : 0}</div>
             <div>km(square)</div>
         </div>
         <hr className={cardDivider} />
