98e1211e75c1e98fcc5cb32a4f5e8cc7ddf30519
Moneexa <14besemsyed@seecs.edu.pk>
2024-08-29 18:15:38 +0200

    cleaned the component

--- src/components/workspace/proposed-solution/ProposedSolution.css.ts
+++ src/components/workspace/proposed-solution/ProposedSolution.css.ts
@@ -1,28 +1,6 @@
-import { style, styleVariants } from "@vanilla-extract/css";
+import { style } from "@vanilla-extract/css";
 
 export const proposedSolutionParent = style({
   display: "flex",
   flexDirection: "column",
-});
-
-export const buttonStyle = style({
-  margin: "5px 0px",
-  cursor: "pointer",
-  padding: "10px",
-  borderRadius: "4px",
-  textAlign: "left",
-});
-
-export const buttonStyleVariant = styleVariants({
-  default: {
-    color: "black",
-    selectors: {
-      "&:hover": {
-        backgroundColor: "lightgray",
-      },
-    },
-  },
-  clicked: {
-    backgroundColor: "#e0e2ec",
-  },
 });
--- src/components/workspace/proposed-solution/ProposedSolution.tsx
+++ src/components/workspace/proposed-solution/ProposedSolution.tsx
@@ -1,62 +1,14 @@
-import { useContext, useRef } from "react"
-import { SolutionsContext } from "../../../store/Solutions"
-import { buttonStyle, buttonStyleVariant, proposedSolutionParent } from "./ProposedSolution.css"
+import { proposedSolutionParent } from "./ProposedSolution.css"
 import { heading } from "../Workspace.css"
-import { FeatureCollection } from "../../../types"
+import SolutionsList from "./solution-widgets/solution-list/SolutionsList"
+import FileUpload from "./solution-widgets/upload-file/FileUpload"
 export default function ProposedSolution() {
-    const { sols, setSelectedSolIndx, selectedSolIndx, addSolution } = useContext(SolutionsContext)
-    const fileInputRef = useRef<HTMLInputElement>(null)
-    const handleSolutionClick = (index: number) => {
-        setSelectedSolIndx(index)
-    }
-
-    const handleFileUploadButtonClick = () => {
-        if (fileInputRef.current) {
-            fileInputRef.current.click();
-        }
-    };
-
-    const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const file = event.target.files?.[0];
-        if (file) {
-            const reader = new FileReader();
-            reader.onload = (e) => {
-                const content = e.target?.result;
-                if (typeof content === 'string') {
-                    try {
-                        const jsonContent: FeatureCollection = JSON.parse(content);
-                        addSolution(jsonContent);
-                    } catch (error) {
-                        console.error('Error parsing JSON:', error);
-                    }
-                }
-            };
-            reader.readAsText(file);
-        }
-    };
     return <div className={proposedSolutionParent}>
         <div className={heading}>Solutions</div>
         <div className={proposedSolutionParent}>
-            {
-                sols.map((_, index) => {
-                    return <div id={`${index}`}
-                        className={`${buttonStyle} ${selectedSolIndx === index ? buttonStyleVariant.clicked : buttonStyleVariant.default}`}
-                        onClick={() =>
-                            handleSolutionClick(index)
-                        }>{"Geo Result" + index}</div>
-                })
-            }
+            <SolutionsList />
+            <FileUpload />
         </div>
-        <button className={`${buttonStyle} ${buttonStyleVariant.default}`} onClick={handleFileUploadButtonClick}>
-            Upload JSON File
-        </button>
-        <input
-            type="file"
-            ref={fileInputRef}
-            style={{ display: 'none' }}
-            accept=".json"
-            onChange={handleFileChange}
-        />
 
     </div >
 }
