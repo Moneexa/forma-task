19902afe378109bdfc881cb536940f3aa6fa0d93
Moneexa <14besemsyed@seecs.edu.pk>
2024-08-29 18:15:23 +0200

    added common styles

--- src/components/workspace/Workspace.css.ts
+++ src/components/workspace/Workspace.css.ts
@@ -1,4 +1,4 @@
-import { style } from "@vanilla-extract/css";
+import { style, styleVariants } from "@vanilla-extract/css";
 
 export const gridParent = style({
   display: "grid",
@@ -11,4 +11,30 @@ export const heading = style({
   fontSize: "1.5em",
   fontWeight: "bold",
   marginBottom: "10px",
+});
+export const buttonStyle = style({
+  all: "unset",
+  textAlign: "center",
+  padding: "10px",
+  borderRadius: "12px",
+  cursor: "pointer",
+  boxShadow:
+    "0px 3px 5px -1px rgba(0, 0, 0, .2), 0px 6px 10px 0px rgba(0, 0, 0, .14), 0px 1px 18px 0px rgba(0, 0, 0, .12);",
+});
+
+export const buttonStyleVariant = styleVariants({
+  default: {
+    color: "darkblue",
+    backgroundColor: "#e0e0ff",
+    selectors: {
+      "&:disabled": {
+        backgroundColor: "#dedbd5",
+        color: "gray",
+      },
+    },
+  },
+  clicked: {
+    backgroundColor: "#a194e3",
+    color: "whitesmoke",
+  },
 });
